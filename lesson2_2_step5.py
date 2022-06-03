a0d82570edbfff098808564c6602a3891d96358b
VitalN1 <vital_8marta@mail.ru>
2022-06-03 12:21:50 +0300

    Добавил процедуру закрытия браузера

--- lesson2_2_step5.py
+++ lesson2_2_step5.py
@@ -5,8 +5,10 @@ from selenium import webdriver
 browser = webdriver.Chrome()
 link = "https://SunInJuly.github.io/execute_script.html"
 browser.get(link)
+# noinspection PyDeprecation
 button = browser.find_element_by_tag_name("button")
 browser.execute_script("return arguments[0].scrollIntoView(true);", button)
 button.click()
 # не забываем оставить пустую строку в конце файла
 
+browser.exit()
