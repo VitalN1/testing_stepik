8cfd612f26e779d9ed55d56a8dcb8836d898c9cd
VitalN1 <vital_8marta@mail.ru>
2022-06-03 12:45:40 +0300

    Добавил задержку перед закрытием И процедуру закрытия

--- lesson2_2_step5.py
+++ lesson2_2_step5.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-
-
+import time
 browser = webdriver.Chrome()
 link = "https://SunInJuly.github.io/execute_script.html"
 browser.get(link)
 # noinspection PyDeprecation
-button = browser.find_element_by_tag_name("button")
+button = browser.find_element_by_tag_name('button')
 browser.execute_script("return arguments[0].scrollIntoView(true);", button)
 button.click()
 # не забываем оставить пустую строку в конце файла
-
-browser.exit()
+time.sleep(10)
+browser.close()
