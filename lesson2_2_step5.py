cc63a670a6ff359dd7c21032d71865723c619057
VitalN1 <vital_8marta@mail.ru>
2022-06-03 14:22:18 +0300

    Удалена неправильная процедура поиска элемента

--- lesson2_2_step5.py
+++ lesson2_2_step5.py
@@ -5,7 +5,8 @@ browser = webdriver.Chrome()
 link = "https://SunInJuly.github.io/execute_script.html"
 browser.get(link)
 # noinspection PyDeprecation
-button = browser.find_element_by_tag_name('button')
+browser.find_element(by=By.TAG_NAME, value=name)
+
 browser.execute_script("return arguments[0].scrollIntoView(true);", button)
 button.click()
 # не забываем оставить пустую строку в конце файла
