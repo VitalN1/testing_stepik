6017815f654d39054d8541e09711513deebf45e4
VitalN1 <vital_8marta@mail.ru>
2022-06-03 16:14:04 +0300

    Изменил процедуру

--- lesson2_2_step5.py
+++ lesson2_2_step5.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
+
 from selenium import webdriver
 import time
-browser = webdriver.Chrome()
-link = "https://SunInJuly.github.io/execute_script.html"
-browser.get(link)
-# noinspection PyDeprecation
-browser.find_element(by=By.TAG_NAME, value='button')
 
-browser.execute_script("return arguments[0].scrollIntoView(true);", button)
-button.click()
-# не забываем оставить пустую строку в конце файла
-time.sleep(10)
-browser.close()
+try:
+    browser = webdriver.Chrome()
+    link = "https://SunInJuly.github.io/execute_script.html"
+    browser.get(link)
+    # noinspection PyDeprecation
+    browser.find_element(by=By.TAG_NAME, value='button')
+
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    button.click()
+    # не забываем оставить пустую строку в конце файла
+    time.sleep(10)
+    browser.close()
+except:
+    browser.close()
